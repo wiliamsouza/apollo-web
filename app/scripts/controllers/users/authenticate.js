453c5594d378b0f5ade4e41e2408d9dc54afa908
Wiliam Souza <wiliamsouza83@gmail.com>
2014-04-09 14:00:47 -0300

    Changed default value from remember to false and added code to get user detail

--- app/scripts/controllers/users/authenticate.js
+++ app/scripts/controllers/users/authenticate.js
@@ -4,20 +4,31 @@ angular.module('apolloApp')
   .controller('UserAuthenticateCtrl', function ($scope, $http, $window, $location) {
     $scope.email = '';
     $scope.password = '';
-    $scope.remember = true;
+    $scope.remember = false;
     $scope.message = '';
     $scope.authenticate = function(isValid) {
       var data = {'email': $scope.email, 'password': $scope.password};
       if (isValid) {
-        $http({method: 'POST', url: 'http://localhost:8000/users/authenticate', data: data}).
+        $http({method: 'POST', url: 'http://127.0.0.1:8000/users/authenticate', data: data}).
           success(function(data, status, headers, config) {
             $window.localStorage.token = data.token;
+
+            $http({method: 'GET', url: 'http://127.0.0.1:8000/users/' + $scope.email}).
+              success(function(data, status, headers, config) {
+                $window.localStorage.APIKey = data.apikey;
+                $window.localStorage.name = data.name;
+              }).
+              error(function(data, status, headers, config) {
+                delete $window.localStorage.token;
+                $scope.message = data;
+              });
+
             $location.path('/');
           }).
           error(function(data, status, headers, config) {
             delete $window.localStorage.token;
             $scope.message = data;
-        });
+          });
       }
     };
   });
