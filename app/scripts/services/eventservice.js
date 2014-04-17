17ba242dc00f4c13cebfe29f39a82f0206bb7315
Wiliam Souza <wiliamsouza83@gmail.com>
2014-04-17 18:09:47 -0300

    Added event serive

---
+++ app/scripts/services/eventservice.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('apolloApp')
+  .factory('EventService', function ($window) {
+
+    var ws = new WebSocket('ws://127.0.0.1:8000/ws/web/' + $window.localStorage.APIKey);
+
+    ws.onopen = function() {
+      console.log('Websocket has been opened!');
+    };
+    
+    ws.onmessage = function(message) {
+      console.log(message);
+    };
+
+    function sendEvent (message) {
+      ws.send(JSON.stringify(message));
+    }
+
+    return {
+      sendEvent: function(message) {
+        sendEvent(message);
+      }
+    };
+  });
